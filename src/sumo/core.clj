b0084bf2732dfe90a7f44cc62410c018c575fad4
Reid Draper <reiddraper@gmail.com>
2011-12-14 21:17:09 -0500

    lein new commit

---
+++ src/sumo/core.clj
@@ -0,0 +1,1 @@
+(ns sumo.core)
---
+++ test/sumo/test/core.clj
@@ -0,0 +1,6 @@
+(ns sumo.test.core
+  (:use [sumo.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
