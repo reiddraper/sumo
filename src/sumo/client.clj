010c703eca9c4195ca879a5f2d15dbbce3fb6064
Reid Draper <reiddraper@gmail.com>
2011-12-14 21:45:55 -0500

    Add `connect` comment

--- src/sumo/client.clj
+++ src/sumo/client.clj
@@ -5,6 +5,9 @@
 (def ^{:private true} default-port 8087)
 
 (defn connect
+  "Return a connection. With no arguments,
+  this returns a connection to localhost
+  at the default protocol buffers port"
   ([] (connect default-host
                default-port))
   ([host port]
