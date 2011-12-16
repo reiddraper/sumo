e3433257dbce538e08fcab46550071bc0f6068f3
Reid Draper <reiddraper@gmail.com>
2011-12-15 20:57:29 -0500

    add license information

--- src/sumo/client.clj
+++ src/sumo/client.clj
@@ -1,3 +1,22 @@
+;; -------------------------------------------------------------------
+;; Copyright (c) 2011 Basho Technologies, Inc.  All Rights Reserved.
+;;
+;; This file is provided to you under the Apache License,
+;; Version 2.0 (the "License"); you may not use this file
+;; except in compliance with the License.  You may obtain
+;; a copy of the License at
+;;
+;;   http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing,
+;; software distributed under the License is distributed on an
+;; "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+;; KIND, either express or implied.  See the License for the
+;; specific language governing permissions and limitations
+;; under the License.
+;;
+;; -------------------------------------------------------------------
+
 (ns sumo.client
   (:refer-clojure :exclude [get])
   (:import (com.basho.riak.client.builders RiakObjectBuilder))
@@ -21,7 +40,7 @@
   (.ping client))
 
 (defn get [client bucketname keyname]
-  (let [results (.fetch c bucketname keyname)
+  (let [results (.fetch client bucketname keyname)
        first-result  (first (seq results))]
     (.getValueAsString first-result)))
 
@@ -31,7 +50,7 @@
                       bucketname keyname)
         riak-object (-> base-object
                      (.withValue value) (.build))]
-    (.store c riak-object)))
+    (.store client riak-object)))
 
 
 (comment
